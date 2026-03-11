d6b1aca7ef6e413ea5d8c3aa655c0a00fff3fb85
Filip Nilsen <filipn@uia.no>
2026-03-11 16:58:14 +0100

    Added vibe code in gdt init

--- src/group_29/src/gdt/gdt.c
+++ src/group_29/src/gdt/gdt.c
@@ -24,6 +24,9 @@ struct gdt_entry create_gdt_entry(
     // Store the high 4 bits of the segment limit
     // (limit bits 16–19)
     r.granularity = (limit >> 16) & 0x0F;
+    r.granularity |= (flags & 0xF0);
+
+    return r;
 }
 
 struct gdt_pointer create_gdt_pointer(struct gdt_entry *start, uint32_t entries)
@@ -41,21 +44,20 @@ void gdt_init() {
     struct gdt_pointer* pp = &p;
 
     asm volatile (
-        // Some asm from here http://www.osdever.net/bkerndev/Docs/gdt.htm
-        ".intel_syntax noprefix  \n\t" // Switch to Intel/NASM style
-        "lgdt [%0]               \n\t" // NASM-style addressing
-        "mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment\n\t"
+        ".intel_syntax noprefix\n\t"
+        "lgdt [%0]\n\t"
+        "mov ax, 0x10\n\t"
         "mov ds, ax\n\t"
         "mov es, ax\n\t"
         "mov fs, ax\n\t"
         "mov gs, ax\n\t"
         "mov ss, ax\n\t"
-        "jmp 0x08:flush2   ; 0x08 is the offset to our code segment: Far jump!\n\t"
-        "flush2:\n\t"
-        "ret               ; Returns back to the C code!\n\t"
-        ".att_syntax             \n\t" // Switch back to keep the compiler happy
-        :
-        : "r" (p)                // Pass the pointer in a register
+        "push 0x08\n\t"       // Push code selector
+        "push offset 1f\n\t"  // Push address of label 1
+        "retf\n\t"            // Far return to reload CS
+        "1:\n\t"
+        ".att_syntax\n\t"
+        : : "r" (&p) : "ax"
     );
 
 
