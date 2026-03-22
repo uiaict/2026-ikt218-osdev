a34ad4140080d0be91bd94a79e0f5b7129a53d10
yasmin <yasminsae@uia.no>
2026-03-22 22:26:06 +0000

    main function updated for memory and PIT

--- src/30_OSDev/src/kernel.c
+++ src/30_OSDev/src/kernel.c
@@ -2,24 +2,84 @@
 #include "terminal.h"
 #include "interrupts/idt.h"
 #include "interrupts/irq.h"
+#include "memory/kernel_memory.h"
+#include "memory/paging.h"
+#include "pit/pit.h"
+#include <stdint.h>               // for uint32_t
+#include "song_player/song_player.h"
+#include "song_player/song.h"
 
+extern uint32_t end; //to find out where the free memory begins
+extern volatile uint32_t tick;
+struct MyClass { int a; int b; };   //dummy class for testing new
+extern void test_new(void);
 
 void main(void)
 {
     gdt_init();                // Set up Global Descriptor Table
     idt_init();                // Set up Interrupt Descriptor Table
-    pic_remap();              // Remap PIC before setting IRQs
+    pic_remap();  
+    init_pit();            // Remap PIC before setting IRQs
     asm volatile("sti");     // ENABLE INTERRUPTS
     
-    terminal_write("Hello World, IRQ setup complete!\n"); // Write to the screen
+    terminal_write("Hello World! \n"); // Write to the screen
 
-    /*asm volatile("int $0x0");  // Trigger interrupt 0
-    asm volatile("int $0x1");  // Trigger interrupt 1
-    asm volatile("int $0x2");  // Trigger interrupt 2 
     
-    asm volatile("int $0x20"); // IRQ0
-    asm volatile("int $0x21"); // IRQ1
-    asm volatile("int $0x22"); // IRQ2 */
-        
-    while(1) { }                     // Halt CPU
+    init_kernel_memory(&end); // Initialize kernel memory manager
+    init_paging(); // Setup paging
+    print_memory_layout(); // See updated heap_end
+
+    
+    // Test malloc
+    void* mem1 = malloc(1024);
+    void* mem2 = malloc(2048);
+
+    terminal_write("mem1: 0x");
+    terminal_write_hex((uint32_t)mem1);
+    terminal_write("\n");
+
+    terminal_write("mem2: 0x");
+    terminal_write_hex((uint32_t)mem2);
+    terminal_write("\n");
+
+    test_new(); // Test C++ new operator
+
+    // Test song player
+    terminal_write("\nStarting music...\n");
+    play_music();
+    terminal_write("Music finished.\n");
+
+
+    // MAIN LOOP
+    int counter = 0;
+
+    while (1)
+    {
+        terminal_write("tick: ");
+        terminal_write_dec(tick);
+        terminal_write("\n");
+
+        terminal_write("[");
+        terminal_write_dec(counter);
+        terminal_write("] Sleeping with busy-waiting (HIGH CPU)\n");
+
+        sleep_busy(1000);
+
+        terminal_write("[");
+        terminal_write_dec(counter);
+        terminal_write("] Slept using busy-waiting\n");
+        counter++;
+
+        terminal_write("[");
+        terminal_write_dec(counter);
+        terminal_write("] Sleeping with interrupts (LOW CPU)\n");
+
+        sleep_interrupt(1000);
+
+        terminal_write("[");
+        terminal_write_dec(counter);
+        terminal_write("] Slept using interrupts\n");
+        counter++;
+    }
+
 }
