22fc583cfe39adcec1c0c18a9a1e5b52b02a52c2
Filip Nilsen <gord1@meme-protocol.click>
2026-04-12 01:02:53 +0200

    This is a bad idea, but I have no better idea.

---
+++ src/group_29/src/keyboard/keyboard.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "stdint.h"
+struct {
+    uint8_t buffer[256];
+    uint8_t* head;
+} typedef scancode_buffer_t;
+
+struct {
+    /** Same as IBM key No. */
+    uint16_t keycode;
+    /** True if the key was released. False means that the key is pressed down. */
+    bool release;
+} typedef keycode_t;
+
+struct {
+    keycode_t buffer[256];
+    keycode_t* head;
+} typedef keycode_buffer_t;
+
+void push_scancode_buffer(scancode_buffer_t* buffer, uint8_t scancode);
+uint8_t pop_scancode_buffer(scancode_buffer_t* buffer);
+scancode_buffer_t create_scancode_buffer();
+
+void push_keycode_buffer(keycode_buffer_t* buffer, keycode_t keycode);
+keycode_t pop_keycode_buffer(keycode_buffer_t* buffer);
+keycode_buffer_t create_keycode_buffer();
+
+void scancode2keycode(scancode_buffer_t* input, keycode_buffer_t* output);
+keycode_t new_keycode(uint16_t keycode, bool release);
