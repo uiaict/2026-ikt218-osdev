18f6f03b33c1fa09b569047d96880cbeb230c526
Fabian Haukedal Johansen <fabianhj@uia.no>
2026-04-27 21:28:35 +0200

    Minimal kernel baseline

---
+++ src/solo_fabian/src/kernel.c
@@ -0,0 +1,15 @@
+typedef unsigned short uint16_t;
+typedef unsigned int size_t;
+
+void kernel_main(void) {
+    volatile uint16_t* vga = (volatile uint16_t*)0xB8000;
+    const char* msg = "IKT218 kernel loaded";
+
+    for (size_t i = 0; msg[i] != '\0'; i++) {
+        vga[i] = (uint16_t)msg[i] | (uint16_t)(0x0F << 8);
+    }
+
+    for (;;) {
+        __asm__ volatile ("hlt");
+    }
+}
