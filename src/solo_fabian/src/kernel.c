52660b3210f58953a78485fe38467cf39bdbc3d0
Fabian <fabianhj@uia.no>
2026-04-27 21:55:10 +0200

    setup bootable kernel baseline

--- src/solo_fabian/src/kernel.c
+++ src/solo_fabian/src/kernel.c
@@ -1,7 +1,7 @@
 typedef unsigned short uint16_t;
 typedef unsigned int size_t;
 
-void kernel_main(void) {
+void main(void) {
     volatile uint16_t* vga = (volatile uint16_t*)0xB8000;
     const char* msg = "IKT218 kernel loaded";
 
