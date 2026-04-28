972b044a9283c59c7f657b1c0035d81f5f80fb0f
Invictus101 <david.haugen21@gmail.com>
2026-04-28 21:42:14 +0000

    GCC change

--- build/91_nineone/cmake_install.cmake
+++ build/91_nineone/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /workspaces/2026-ikt218-osdev/src/91_solo_nineone
+# Install script for directory: /workspaces/2026-ikt218-osdev/src/91_nineone
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/workspaces/2026-ikt218-osdev/build/91_solo_nineone/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/workspaces/2026-ikt218-osdev/build/91_nineone/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
