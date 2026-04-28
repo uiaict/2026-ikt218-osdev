c04099d68c3c2262ac6df0dbab78f9b43e01314c
David Alexander Danielsen <gruveexp123@gmail.com>
2026-04-28 21:56:18 +0000

    make kernel.c better

--- src/91_nineone/src/kernel.c
+++ src/91_nineone/src/kernel.c
@@ -1,5 +1,16 @@
-void main(void)
-{
-    while (1) {
+int main() {
+    char *video_memory = (char*) 0xB8000; // startadresse for VGA tekstmodus
+
+    const char *str = "Velkommen til DaviDOS!";
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        // 2x: char, 2x+1: attributt (farge)
+        video_memory[i * 2] = str[i];
+        video_memory[i * 2 + 1] = 0x1F; 
     }
+
+    while (1) { // coming soon
+    }
+
+    return 0;
 }
